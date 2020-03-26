b929641030f64a05bd016bf0eb7d955265f7344d
Matheus Wendel <48329681+Matheuswrs@users.noreply.github.com>
2020-03-26 18:51:21 -0300

    Adiciona Frontend e Backend da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const connection = require('./database/connection');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+//Session Routes
+routes.post('/sessions', SessionController.create);
+
+// Ongs Routes
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+// Profile Routes
+routes.get('/profile', ProfileController.index);
+
+// Incidents Routes
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
